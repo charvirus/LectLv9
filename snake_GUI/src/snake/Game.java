eaa7a69e442c76dcb5bca39bef0ee829d509ea11
Brandon J <43872889+charvirus@users.noreply.github.com>
2021-11-02 15:18:58 +0900

    #15 몸 구현

--- snake_GUI/src/snake/Game.java
+++ snake_GUI/src/snake/Game.java
@@ -11,8 +11,9 @@ class SnakePanel extends MyUtil {
 	private final int BUTTON = 50;
 	private Rect[] snake;
 	private JButton[] btn = new JButton[4];
-	private int length; 
+	private int length;
 	private int dir = 0;
+
 	public SnakePanel() {
 		setLayout(null);
 		setBounds(0, 0, 1000, 800);
@@ -20,7 +21,8 @@ class SnakePanel extends MyUtil {
 		setMap();
 		setSnake();
 		setBtn();
-
+		setFocusable(true);
+		addKeyListener(this);
 	}
 
 	private void setBtn() {
@@ -31,7 +33,7 @@ class SnakePanel extends MyUtil {
 			this.btn[i] = new JButton();
 			this.btn[i].setBounds(x, y, BUTTON, BUTTON);
 			this.btn[i].setText(text[i]);
-			this.btn[i].addActionListener(this);
+			this.btn[i].addMouseListener(this);
 			add(this.btn[i]);
 			x += BUTTON;
 			if (i == this.btn.length - 2) {
@@ -64,14 +66,17 @@ class SnakePanel extends MyUtil {
 	}
 
 	private void setSnake() {
+		int x = 1000 / 2 - BUTTON * 10 / 2 - 100 + BUTTON * 3;
+		int y = 800 / 2 - BUTTON * 10 / 2 + BUTTON * 3;
 		snake = new Rect[4];
 		for (int i = 0; i < snake.length; i++) {
-			this.snake[i] = new Rect((3 + i) * BUTTON, BUTTON, BUTTON, BUTTON);
+			this.snake[i] = new Rect(x, y, BUTTON, BUTTON);
 			if (i == 0) {
 				snake[i].setC(Color.green);
 			} else {
 				snake[i].setC(Color.cyan);
 			}
+			x += BUTTON;
 		}
 	}
 
@@ -99,7 +104,6 @@ class SnakePanel extends MyUtil {
 				}
 			}
 		}
-
 		for (int i = 0; i < snake.length; i++) {
 			g.setColor(snake[i].getC());
 			g.drawRect(snake[i].getX() - snake[i].getWidth() / 2, snake[i].getY() - snake[i].getHeight() / 2,
@@ -107,34 +111,73 @@ class SnakePanel extends MyUtil {
 			g.fillRect(snake[i].getX() - snake[i].getWidth() / 2, snake[i].getY() - snake[i].getHeight() / 2,
 					snake[i].getWidth(), snake[i].getHeight());
 		}
-		
+
 		repaint();
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		// TODO Auto-generated method stub
 
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+	private void update() {
+		int x = 1000 / 2 - BUTTON * 10 / 2 - 100;
+		int y = 800 / 2 - BUTTON * 10 / 2;
+		int xx = snake[0].getX();
+		int yy = snake[0].getY();
+		boolean check = true;
+		if (dir == 1) {
+			xx -= BUTTON;
+		} else if (dir == 2) {
+			yy += BUTTON;
+		} else if (dir == 3) {
+			xx += BUTTON;
+		} else if (dir == 4) {
+			yy -= BUTTON;
+		}
+		
+		if (check||xx < x || xx > x + BUTTON * 10 || yy < y || yy > y + BUTTON * 10) {
+			xx = snake[0].getX();
+			yy = snake[0].getY();
+		} else {
+			for (int i = snake.length - 1; i > 0; i--) {
+				snake[i].setX(snake[i - 1].getX());
+				snake[i].setY(snake[i - 1].getY());
+			}
+			snake[0].setX(xx);
+			snake[0].setY(yy);
+		}
 
 	}
-	
+
 	@Override
-	public void actionPerformed(ActionEvent e) {
-		if(e.getSource() == this.btn[0]) {
+	public void keyTyped(KeyEvent e) {
+
+		if (e.getKeyChar() == 'a') {
 			this.dir = 1;
-		}else if ( e.getSource() == this.btn[1]) {
+		} else if (e.getKeyChar() == 's') {
 			this.dir = 2;
-		}else if ( e.getSource() == this.btn[2]) {
+		} else if (e.getKeyChar() == 'd') {
 			this.dir = 3;
-		}else if ( e.getSource() == this.btn[3]) {
+		} else if (e.getKeyChar() == 'w') {
 			this.dir = 4;
 		}
+		update();
+		this.dir = 0;
 	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		if (e.getSource() == this.btn[0]) {
+			this.dir = 1;
+		} else if (e.getSource() == this.btn[1]) {
+			this.dir = 2;
+		} else if (e.getSource() == this.btn[2]) {
+			this.dir = 3;
+		} else if (e.getSource() == this.btn[3]) {
+			this.dir = 4;
+		}
+
+		update();
+		this.dir = 0;
+	}
+
 }
 
 public class Game extends JFrame {
