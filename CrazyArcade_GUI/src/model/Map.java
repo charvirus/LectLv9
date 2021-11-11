fe8b33d43144d845e37b2cd0431ccce73e131a40
Administrator <Administrator@205-20>
2021-11-11 15:20:24 +0900

    #42 맵 클래스 만드는중

--- CrazyArcade_GUI/src/model/Map.java
+++ CrazyArcade_GUI/src/model/Map.java
@@ -1,5 +1,51 @@
 package model;
 
-public class Map {
+import javax.swing.ImageIcon;
 
+public class Map {
+	public static final int PLAYER = 1;
+	public static final int BOMB = 2;
+	public static final int WALL = 3;
+	public static final int ROAD = 4;
+	public static final int ITEM = 5;
+	private int state;
+	private int x, y, w, h;
+	private String names[] = {"bazzi","bomb","brick","grass","item"};
+	private String fileName;
+	private ImageIcon image;
+	
+	
+	public Map(int state, int x, int y, int w, int h) {
+		this.state = state;
+		this.x = x;
+		this.y = y;
+		this.w = w;
+		this.h = h;
+	}
+	public int getState() {
+		return state;
+	}
+	public void setState(int state) {
+		this.state = state;
+	}
+	public int getX() {
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	public int getW() {
+		return w;
+	}
+	public int getH() {
+		return h;
+	}
+	public String getFileName() {
+		return fileName;
+	}
+	public ImageIcon getImage() {
+		return image;
+	}
+
+	
 }
