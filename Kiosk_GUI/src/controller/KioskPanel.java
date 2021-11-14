4f8cb1cc4bd1c28513e797b279f35cb78cee253f
Brandon J <43872889+charvirus@users.noreply.github.com>
2021-11-15 01:12:33 +0900

    #46 버튼 해야됨 ...

--- Kiosk_GUI/src/controller/KioskPanel.java
+++ Kiosk_GUI/src/controller/KioskPanel.java
@@ -1,5 +1,17 @@
 package controller;
 
-public class KioskPanel {
+import models.MyUtil;
 
+public class KioskPanel extends MyUtil {
+	public KioskPanel() {
+		setLayout(null);
+		setBounds(50, 50, 500, 700);
+		setButton();
+		setFocusable(true);
+		addKeyListener(this);
+	}
+
+	private void setButton() {
+		
+	}
 }
